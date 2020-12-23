97bf6a3cc242d3687492afda5a2c5ca131a1b767
Deepak Guptha <vzan2012@gmail.com>
2020-12-23 15:08:10 +0530

    feat: Updating Middleware and types

--- src/app.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var express = require("express");
-var app = express();
-// const port = 3000 || process.env.PORT
-app.listen(3000);
--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,14 @@
-import express from 'express'
+import express, { Request, Response, NextFunction } from 'express'
+import todoRoutes from './routes/todos'
 
 const app = express();
 
 const port = 3000 || process.env.PORT
+
+app.use('/todos', todoRoutes)
+
+app.use((err: Error, req: Request, resp: Response, next: NextFunction) => {
+    resp.status(500).json({ message: err.message })
+})
 
 app.listen(port)
